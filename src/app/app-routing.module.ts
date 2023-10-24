014c0bae2dfcf014c0931b2004e09e555d6e8f44
Ivan Espinosa <ivarespar@gmail.com>
2023-10-23 22:39:07 -0500

    [o22] Carga perezosa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeModule } from './welcome/welcome.module';
 
 // aqui se definen todas las rutas
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren:() => import('./welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path: 'courses',
+    loadChildren:() => import('./courses/courses.module').then(m => m.CoursesModule)
+  }
+];
 
 @NgModule({
   declarations: [],
